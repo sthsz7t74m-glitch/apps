5e6b97664f1fa970735036a92797792fdd8b9ac7
sthsz7t74m-glitch <304956452+sthsz7t74m-glitch@users.noreply.github.com>
2026-07-21 22:47:11 +0900

    Refresh PWA cache for size controls

--- number-tap-25/sw.js
+++ number-tap-25/sw.js
@@ -1,1 +1,32 @@
-const CACHE_NAME='number-tap-25-v2';const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)))});self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))),self.clients.claim()]))});self.addEventListener('fetch',event=>event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request))))
+const CACHE_NAME='number-tap-25-v3';
+const ASSETS=[
+  './',
+  './index.html',
+  './style.css?v=1.1.1',
+  './app.js?v=1.1.1',
+  './manifest.webmanifest?v=1.1.1',
+  './icon.svg'
+];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))),
+    self.clients.claim()
+  ]));
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(
+    fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html')))
+  );
+});

c2d3fd396650ef9b678e9f23018252595af186f8
sthsz7t74m-glitch <304956452+sthsz7t74m-glitch@users.noreply.github.com>
2026-07-27 23:51:56 +0900

    Add generated AV directory data placeholder

---
+++ av-actress-directory/data-live.js
@@ -0,0 +1,5 @@
+(() => {
+  'use strict';
+  window.AV_ACTRESSES = Array.isArray(window.AV_ACTRESSES) ? window.AV_ACTRESSES : [];
+  window.AV_DIRECTORY_META = window.AV_DIRECTORY_META || {};
+})();

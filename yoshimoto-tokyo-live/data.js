3dcb9eaabda00cb173e93865be9f34bbc430ea3d
sthsz7t74m-glitch <304956452+sthsz7t74m-glitch@users.noreply.github.com>
2026-07-28 08:36:01 +0900

    Add expanded Yoshimoto Tokyo live data

---
+++ yoshimoto-tokyo-live/data.js
@@ -0,0 +1,60 @@
+window.YOSHIMOTO_LIVE_META = Object.freeze({
+  version: "1.2.0",
+  updatedAt: "2026-07-28",
+  sourceName: "FANYチケット・吉本各劇場公式情報",
+  note: "販売中表示はチケット受付状態です。残席数・出演者変更は公式ページで最終確認してください。"
+});
+
+window.YOSHIMOTO_LIVE_ROWS = String.raw`
+2026-07-28|11:30|12:00|ルミネtheよしもと|新宿|ルミネtheよしもと 夏休み特別公演プラス＜1回目＞|yose|available|4000|一般 4,000円／小学生以下 3,000円|ジャルジャル／レイザーラモン／もりやすバンバンビガロ／ガクテンソク／スカチャン／ダイタク／ビスケットブラザーズ／今井らいぱち／ヨネダ2000|https://ticket.fany.lol/lumine
+2026-07-28|13:30|14:00|ルミネtheよしもと|新宿|ルミネtheよしもと 夏休み特別公演プラス＜2回目＞|yose|available|4000|一般 4,000円／小学生以下 3,000円|ジャルジャル／レイザーラモン／もりやすバンバンビガロ／ガクテンソク／スカチャン／ダイタク／ビスケットブラザーズ／今井らいぱち／ヨネダ2000|https://ticket.fany.lol/lumine
+2026-07-28|15:30|16:00|ルミネtheよしもと|新宿|ルミネtheよしもと 夏休み特別公演プラス＜3回目＞|yose|available|4000|一般 4,000円／小学生以下 3,000円|ジャルジャル／レイザーラモン／もりやすバンバンビガロ／ガクテンソク／スカチャン／ダイタク／ビスケットブラザーズ／今井らいぱち／ヨネダ2000|https://ticket.fany.lol/lumine
+2026-07-28|17:30|18:00|ルミネtheよしもと|新宿|ルミネtheよしもと 夏休み特別公演プラス＜4回目＞|yose|available|4000|一般 4,000円／小学生以下 3,000円|ジャルジャル／レイザーラモン／もりやすバンバンビガロ／ガクテンソク／スカチャン／ダイタク／ビスケットブラザーズ／今井らいぱち／ヨネダ2000|https://ticket.fany.lol/lumine
+2026-07-28|17:00|17:15|渋谷よしもと漫才劇場|渋谷|渋谷Kiwami極LIVEプラス＋|neta-corner|available|1500|前売 1,500円|おしみんまる／ツーナッカン／大谷健太／イチキップリン／シマッシュレコード／小森園ひろし／アイロンヘッド／カーネーション／たつろう／デンゼル／光永／カゲヤマ|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-07-28|17:15|17:30|神保町よしもと漫才劇場|神保町|神保町Kakeru翔SP|neta|available|1500|前売 1,500円|さや香／からし蓮根／そいつどいつ／ミカボ／大王／イワサキ／エグい速さ／ウィークリー習慣／ドンココ|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-07-28|21:00|21:15|渋谷よしもと漫才劇場|渋谷|ホットフレッシュリリース|project|available||価格は公式で確認|ケビンス／カベポスター／ド桜|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-07-29|11:30|12:00|ルミネtheよしもと|新宿|ルミネtheよしもと 夏休み特別公演＜1回目＞|yose|available|5000|一般 5,000円|村上ショージ／2丁拳銃／佐久間一行／５ＧＡＰ／ガクテンソク／すゑひろがりず／カベポスター／たくろう|https://ticket.fany.lol/lumine
+2026-07-29|13:30|14:00|ルミネtheよしもと|新宿|ルミネtheよしもと 夏休み特別公演＜2回目＞|yose|available|5000|一般 5,000円|村上ショージ／2丁拳銃／佐久間一行／５ＧＡＰ／ガクテンソク／すゑひろがりず／カベポスター／たくろう|https://ticket.fany.lol/lumine
+2026-07-29|15:30|16:00|ルミネtheよしもと|新宿|ルミネtheよしもと 夏休み特別公演＜3回目＞|yose|available|5000|一般 5,000円|村上ショージ／2丁拳銃／佐久間一行／５ＧＡＰ／ガクテンソク／すゑひろがりず／カベポスター／たくろう|https://ticket.fany.lol/lumine
+2026-07-29|17:00|17:15|渋谷よしもと漫才劇場|渋谷|渋谷Kiwami極LIVE|neta-corner|available|1500|前売 1,500円|カベポスター／ヨネダ2000／今井らいぱち／からし蓮根／ラニーノーズ／世間知らズ／ペッパーGuys／スパジーニ／インテイク|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-07-29|17:15|17:30|神保町よしもと漫才劇場|神保町|神保町Kakeru翔SP|neta|available|1500|前売 1,500円|ダンビラムーチョ／九条ジョー／ブラゴーリ／ミカボ／しんや／ゼロカラン／兄弟／ド天国／とりせん|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-07-29|19:00|19:15|渋谷よしもと漫才劇場|渋谷|大黄金世代|project|available||価格は公式で確認|からし蓮根／そいつどいつ／田津原理音／ラニーノーズ／レインボー|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-07-29|19:00|19:15|神保町よしもと漫才劇場|神保町|やさしいズタイの「庭」|project|available||価格は公式で確認|やさしいズ タイ／アイロンヘッド 辻井亮平／ダンビラムーチョ／滝音／ミカボ／ロジャー沖縄|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-07-30|17:00|17:15|渋谷よしもと漫才劇場|渋谷|渋谷Kiwami極LIVE|neta-corner|available|1500|前売 1,500円|ゆにばーす／カゲヤマ／マルセイユ／ランパンプス／ドッチモドッチ／ブラゴーリ／9番街レトロ／ソマオ・ミートボール|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-07-30|17:15|17:30|神保町よしもと漫才劇場|神保町|神保町Kakeru翔LIVE|neta|available|1300|前売 1,300円|小虎／狛犬／DOG FOOD PARTY／フジーズ／ボブのコーラ／ウィークリー習慣／伝書鳩／なでなでボーイ／ただならぬ刀|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-07-30|19:00|19:15|渋谷よしもと漫才劇場|渋谷|パナクポ|project|available||価格は公式で確認|ソマオ・ミートボール／9番街レトロ／ド桜|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-07-30|19:00|19:15|神保町よしもと漫才劇場|神保町|ただならぬ刀企画『ゴリラパンチ』|project|available||価格は公式で確認|ただならぬ刀／ナイチンゲールダンス／ボブのコーラ／そいそ～す|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-07-31|17:00|17:15|渋谷よしもと漫才劇場|渋谷|渋谷Kiwami極LIVE|neta-corner|available|1500|前売 1,500円|ダブルヒガシ／エバース／CITY／TEAM近藤／素敵じゃないか／ミカボ／オフローズ／シンクロニシティ／ピュート|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-07-31|17:15|17:30|神保町よしもと漫才劇場|神保町|神保町Kakeru翔LIVE|neta|available|1300|前売 1,300円|大王／定点計画／アリハガ／エグい速さ／キイロイゾウサン／セカンドロット／ボブのコーラ／ナユタ／ミナミナミ|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-08-01|10:45|11:00|渋谷よしもと漫才劇場|渋谷|渋谷マンゲキお笑いライブ|neta-corner|available|2800|前売 2,800円|蛙亭／カベポスター／やさしいズ／ケビンス／田津原理音／シンクロニシティ／セカンドロット／ライス／GAG|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-08-01|12:30|12:45|渋谷よしもと漫才劇場|渋谷|渋谷マンゲキお笑いライブ|neta-corner|available|2800|前売 2,800円|蛙亭／カベポスター／やさしいズ／ケビンス／田津原理音／シンクロニシティ／セカンドロット／ライス／GAG|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-08-01|14:15|14:30|渋谷よしもと漫才劇場|渋谷|渋谷マンゲキお笑いライブ|neta-corner|available|2800|前売 2,800円|蛙亭／カベポスター／やさしいズ／ケビンス／田津原理音／シンクロニシティ／セカンドロット／ライス／GAG|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-08-01|10:45|11:00|神保町よしもと漫才劇場|神保町|神保町マンゲキお笑いライブ|neta|available|2500|前売 2,500円|ダブルヒガシ／エルフ／九条ジョー／アメリカンBBチキン／ミカボ／オフローズ／マリーマリー／金魚番長／定点計画／ド天国／パパパジャマ|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-08-01|12:45|13:00|神保町よしもと漫才劇場|神保町|神保町マンゲキお笑いライブ|neta|available|2500|前売 2,500円|ダンビラムーチョ／ダブルヒガシ／エルフ／九条ジョー／アメリカンBBチキン／ミカボ／オフローズ／マリーマリー／金魚番長／定点計画／とりせん|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-08-01|14:45|15:00|神保町よしもと漫才劇場|神保町|神保町マンゲキお笑いライブ|neta|available|2500|前売 2,500円|ダンビラムーチョ／ダブルヒガシ／エルフ／CITY／アメリカンBBチキン／ミカボ／オフローズ／マリーマリー／サンタモニカ／金魚番長／とりせん|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-08-02|10:45|11:00|渋谷よしもと漫才劇場|渋谷|渋谷マンゲキお笑いライブ|neta-corner|available|2800|前売 2,800円|ダブルヒガシ／キンボシ／鶴亀／からし蓮根／そいつどいつ／ミカボ／イチゴ／オズワルド／ザ・パンチ|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-08-02|12:30|12:45|渋谷よしもと漫才劇場|渋谷|渋谷マンゲキお笑いライブ|neta-corner|available|2800|前売 2,800円|ダブルヒガシ／キンボシ／鶴亀／からし蓮根／そいつどいつ／ミカボ／イチゴ／オズワルド／ザ・パンチ|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-08-02|14:15|14:30|渋谷よしもと漫才劇場|渋谷|渋谷マンゲキお笑いライブ|neta-corner|available|2800|前売 2,800円|ダブルヒガシ／キンボシ／鶴亀／からし蓮根／そいつどいつ／ミカボ／イチゴ／オズワルド／ザ・パンチ|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-08-02|10:45|11:00|神保町よしもと漫才劇場|神保町|神保町マンゲキお笑いライブ|neta|available|2500|前売 2,500円|滝音／今井らいぱち／くらげ／シモリュウ／素敵じゃないか／ブラゴーリ／ピュート／ゼロカラン／大王／セカンドロット|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-08-02|12:45|13:00|神保町よしもと漫才劇場|神保町|神保町マンゲキお笑いライブ|neta|available|2500|前売 2,500円|滝音／今井らいぱち／くらげ／シモリュウ／素敵じゃないか／ブラゴーリ／ピュート／ゼロカラン／大王／セカンドロット|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-08-02|14:45|15:00|神保町よしもと漫才劇場|神保町|神保町マンゲキお笑いライブ|neta|available|2500|前売 2,500円|滝音／今井らいぱち／くらげ／シモリュウ／素敵じゃないか／ブラゴーリ／ピュート／ゼロカラン／大王／セカンドロット|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-08-05|17:15|17:30|神保町よしもと漫才劇場|神保町|神保町Kakeru翔LIVE|neta|available|1300|前売 1,300円|大王／青いデルタ／イワサキ／ザンゾウ／伝書鳩／ド天国／とりせん／照山おうちごはん|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-08-08|10:45|11:00|渋谷よしもと漫才劇場|渋谷|渋谷マンゲキお笑いライブ お盆SP|neta-corner|available|3300|前売 3,300円|蛙亭／滝音／ダブルヒガシ／エルフ／ヨネダ2000／kento fukaya／太鵬／ドンココ／ビスケットブラザーズ|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-08-08|10:45|11:00|神保町よしもと漫才劇場|神保町|神保町マンゲキお笑いライブ お盆SP|neta|available|2800|前売 2,800円|ゆにばーす／カベポスター／カラタチ／九条ジョー／軟水／鉄人小町／イチゴ／金魚番長／ネイビーズアフロ|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-08-08|12:45|13:00|神保町よしもと漫才劇場|神保町|神保町マンゲキお笑いライブ お盆SP|neta|available|2800|前売 2,800円|ゆにばーす／カベポスター／カラタチ／九条ジョー／軟水／鉄人小町／イチゴ／金魚番長／ネイビーズアフロ|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-08-08|14:45|15:00|神保町よしもと漫才劇場|神保町|神保町マンゲキお笑いライブ お盆SP|neta|available|2800|前売 2,800円|ゆにばーす／カベポスター／カラタチ／九条ジョー／軟水／鉄人小町／イチゴ／金魚番長／ネイビーズアフロ|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-08-09|10:45|11:00|神保町よしもと漫才劇場|神保町|神保町マンゲキお笑いライブ お盆SP|neta|available|2800|前売 2,800円|ヨネダ2000／シカゴ実業／スクラップス／田津原理音／スーパーサイズ・ミー／世間知らズ／伝書鳩／ドンココ|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-08-10|16:45|17:00|渋谷よしもと漫才劇場|渋谷|渋谷Kiwami極LIVE 夏休みSP|neta-corner|available|2000|前売 2,000円|シカゴ実業／デニス／くらげ／からし蓮根／九条ジョー／ケビンス／ブラゴーリ／オフローズ／シンクロニシティ|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-08-15|14:15|14:30|渋谷よしもと漫才劇場|渋谷|渋谷マンゲキお笑いライブ お盆SP|neta-corner|available|3300|前売 3,300円|カベポスター／今井らいぱち／ケビンス／そいつどいつ／オフローズ／ド桜／金魚番長／Dr.ハインリッヒ／ツートライブ／ななまがり|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-08-19|17:00|17:15|渋谷よしもと漫才劇場|渋谷|渋谷Kiwami極LIVEプラス＋|neta-corner|available|1500|前売 1,500円|エバース／セッツァー／鶴亀／アメリカンBBチキン／ブラゴーリ／シンクロニシティ／ソマオ・ミートボール／ピュート|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-08-20|17:15|17:30|神保町よしもと漫才劇場|神保町|神保町Kakeru翔LIVE|neta|available|1300|前売 1,300円|イチゴ／大王／兄弟／ザンゾウ／伝書鳩／とりせん／パパパジャマ|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-08-21|13:30|14:00|ルミネtheよしもと|新宿|ルミネtheよしもと 夏休み特別公演プラス＜2回目＞|yose|available|4000|一般 4,000円|霜降り明星／トータルテンボス／もりやすバンバンビガロ／ライス／サルゴリラ／吉田たち／相席スタート／オズワルド／ダブルヒガシ|https://ticket.fany.lol/lumine
+2026-08-21|15:30|16:00|ルミネtheよしもと|新宿|ルミネtheよしもと 夏休み特別公演プラス＜3回目＞|yose|available|4000|一般 4,000円|霜降り明星／トータルテンボス／もりやすバンバンビガロ／ライス／サルゴリラ／吉田たち／相席スタート／オズワルド／ダブルヒガシ|https://ticket.fany.lol/lumine
+2026-08-22|12:30|12:45|渋谷よしもと漫才劇場|渋谷|渋谷マンゲキお笑いライブ|neta-corner|available|2800|前売 2,800円|滝音／ヨネダ2000／シカゴ実業／CITY／カラタチ／やさしいズ／太鵬／イチゴ／スカチャン／相席スタート|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-08-22|13:00|13:30|ルミネtheよしもと|新宿|ルミネtheよしもと 夏休み特別公演＜2回目＞|yose|available||価格は公式で確認|パンクブーブー／佐久間一行／ハイキングウォーキング／ギャロップ／銀シャリ／ななまがり／ゆにばーす／ガベジ／FUJIWARA 原西孝幸／大山英雄／兼光タカシ／赤松新|https://ticket.fany.lol/lumine
+2026-08-22|15:30|16:00|ルミネtheよしもと|新宿|ルミネtheよしもと 夏休み特別公演＜3回目＞|yose|available||価格は公式で確認|パンクブーブー／佐久間一行／ハイキングウォーキング／ギャロップ／銀シャリ／ななまがり／ゆにばーす／ガベジ／FUJIWARA 原西孝幸／大山英雄／兼光タカシ／赤松新|https://ticket.fany.lol/lumine
+2026-08-23|14:45|15:00|神保町よしもと漫才劇場|神保町|神保町マンゲキお笑いライブ|neta|available|2500|前売 2,500円|蛙亭／kento fukaya／鶴亀／まちるだ／ワラバランス／ケビンス／インテイク／イチゴ／ボブのコーラ／伝書鳩|https://ticket.fany.lol/jimbocho_manzaigekijyo
+2026-08-23|10:30|11:00|ルミネtheよしもと|新宿|ルミネtheよしもと 夏休み特別公演＜1回目＞|yose|available||価格は公式で確認|フットボールアワー／ショウショウ／シャンプーハット／あべこうじ／ギャロップ／５ＧＡＰ／アインシュタイン／ネルソンズ／ラニーノーズ／バッテリィズ／3時のヒロイン|https://ticket.fany.lol/lumine
+2026-08-30|14:15|14:30|渋谷よしもと漫才劇場|渋谷|渋谷マンゲキお笑いライブ|neta-corner|available|2800|前売 2,800円|ドンデコルテ／めぞん／からし蓮根／9番街レトロ／軟水／ピュート／ゼロカラン／サルゴリラ／しずる|https://ticket.fany.lol/shibuya_manzaigekijyo
+2026-08-30|10:30|11:00|ルミネtheよしもと|新宿|ルミネtheよしもと 夏休み特別公演＜1回目＞|yose|available|5000|一般 5,000円|はりけ～んず／佐久間一行／ハイキングウォーキング／バイク川崎バイク／ジェラードン／ネルソンズ／マルセイユ／ドンデコルテ／バッテリィズ／ガベジ／ほんこん／シベリア文太／大山英雄／チャド・マレーン／鈴木那奈|https://ticket.fany.lol/lumine
+`.trim();
